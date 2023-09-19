2f60f3621085463a7ac05910639ff37f6ebe27f1
EricNorrwing <Eric.Norrwing@gmail.com>
2023-09-19 15:09:51 +0200

    Task #3

--- src/com/ericnorrwing/lektion_5_tasks/Main.java
+++ src/com/ericnorrwing/lektion_5_tasks/Main.java
@@ -6,12 +6,17 @@ public class Main {
         //Task #1
         System.out.println("Player #1: " + inputScanner.setName());
 
-        //Task 2
+        //Task #2
         int loopAmount = inputScanner.setValue();
         for(int i = 0;i < loopAmount; i++){
             System.out.println("Loop counter: " + (i+1) + " WEEEEEEEE");
         }
-        
+
+        //Task #3
+        do{
+            System.out.println("Just put in any value, nothing matters and the loop is sad");
+            inputScanner.setValue();
+        } while(true);
 
 
 
