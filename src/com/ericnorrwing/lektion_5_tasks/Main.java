950818d9bd9c74a13c1a2a64038a53a36efffdc5
EricNorrwing <Eric.Norrwing@gmail.com>
2023-09-19 15:13:46 +0200

    Task #4

--- src/com/ericnorrwing/lektion_5_tasks/Main.java
+++ src/com/ericnorrwing/lektion_5_tasks/Main.java
@@ -13,14 +13,26 @@ public class Main {
         }
 
         //Task #3
+        /*
         do{
             System.out.println("Just put in any value, nothing matters and the loop is sad");
             inputScanner.setValue();
         } while(true);
+        */
+
+        //Task 4
+        //re-using "setName" because im lazy, if you feed "stop" it will stop.
+        //it will ask for your name though so its very rude to answer stop
+        boolean isPlaying = true;
+        do{
+            if (inputScanner.setName().equals("stop")){
+                isPlaying = false;
+            }
 
 
+        }while(isPlaying);
 
-
+        
 
     }
 }
